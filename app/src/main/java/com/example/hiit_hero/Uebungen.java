93da5ee201685664767ee13ad3ca356cfc2c81ff
baumi2011 <baumgartnerjjulian@gmx.at>
2025-06-24 20:44:37 +0200

    Eingefügte Gifs

---
+++ app/src/main/java/com/example/hiit_hero/Uebungen.java
@@ -0,0 +1,46 @@
+package com.example.hiit_hero;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Uebungen extends AppCompatActivity {
+    private final String[] allExercises = new String[]{
+            "Liegestütze", "Plank", "Jumping Jacks", "Burpees", "Kniebeugen", "Mountain Climbers"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_uebungen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.uebungen_main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ListView listView = findViewById(R.id.uebungenListView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, allExercises);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedExercise = allExercises[position];
+                Intent intent = new Intent(Uebungen.this, UebungAnsicht.class);
+                intent.putExtra("EXERCISE_NAME", selectedExercise);
+                startActivity(intent);
+            }
+        });
+    }
+} 
